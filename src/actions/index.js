8b4ae9cc09bbf6d4ac6eb607ea8ee2967dc57c5c
Robert Kirby <robertaxelkirby@gmail.com>
2018-02-28 10:16:32 +0000

    remove unnecessary parentheses

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ export const restartGame = () => ({
 })
 
 export const MAKE_GUESS = 'MAKE_GUESS';
-export const makeGuess = (guess) => ({
+export const makeGuess = guess => ({
   type: MAKE_GUESS,
   guess
 })
