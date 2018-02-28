8d22b2842e726023d23dad697f9d07b4184e6fae
Robert Kirby <robertaxelkirby@gmail.com>
2018-02-28 10:15:10 +0000

    fix typos

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,15 @@
-const RESTART_GAME = 'RESTART_GAME';
+export const RESTART_GAME = 'RESTART_GAME';
 export const restartGame = () => ({
   type: RESTART_GAME
 })
 
-const MAKE_GUESS = 'MAKE_GUESS';
+export const MAKE_GUESS = 'MAKE_GUESS';
 export const makeGuess = (guess) => ({
   type: MAKE_GUESS,
   guess
 })
 
-const GENERATE_AURAL_UPDATE = 'GENERATE_AURAL_UPDATE';
-const generateAuralUpdate = () => {
+export const GENERATE_AURAL_UPDATE = 'GENERATE_AURAL_UPDATE';
+export const generateAuralUpdate = () => {
   type: GENERATE_AURAL_UPDATE
 }
